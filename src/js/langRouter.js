a60f2c9836b23f56070e5597b4661107b5fcc28d
iMath97 <camusmathias97@gmail.com>
2020-06-15 15:11:32 +0200

    initial

---
+++ src/js/langRouter.js
@@ -0,0 +1,58 @@
+let lang;
+let enBTN;
+let nlBTN;
+
+getCookie();
+
+if(document.title == "router"){
+    if(lang != "nl" && lang != "en"){
+        setCookie("nl");
+        location.href = "/nl/home.html";
+    } else {
+        routeVisitor();
+    }
+}
+
+function getCookie(){
+    let cookie = document.cookie.split("=");
+    lang = cookie[1];
+    console.log(lang);
+}
+
+function setCookie(lang){
+    document.cookie = `lang=${lang}; expires=2025 12:00:00 UTC; path=/`;
+}
+
+function routeVisitor(){
+    if(lang == "nl"){
+        location.href = "/nl/home";
+    } else if(lang == "en"){
+        location.href = "/en/home";
+    }
+}
+
+function getPath(){
+    let path = location.pathname.split("/");
+    return path[1];
+}
+
+
+if(getPath() == "nl"){
+    enBTN = document.getElementById("enBTN");
+
+    enBTN.addEventListener("click", () => {
+        setCookie("en");
+        console.log("click");
+        location.href = "/en/home";
+    })
+} else if(getPath() == "en"){
+    nlBTN = document.getElementById("nlBTN");
+
+    nlBTN.addEventListener("click", () => {
+        setCookie("nl");
+        location.href = "/nl/home";
+    })
+}
+
+
+
